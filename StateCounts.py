d2301d5f6590ad4cdd83d52febf0690acfd850c1
zackwitz <zackwitz@gmail.com>
2020-11-06 11:34:18 -0500

    Only prints when votes have changed

--- StateCounts.py
+++ StateCounts.py
@@ -11,36 +11,55 @@ states = [
     # for some reason Nevada is causing an error
     # 'Nevada'
 ]
-TIME_INTERVAL = 60  # in seconds, change to how often you want it to update
+TIME_INTERVAL = 60 * 5  # in seconds, change to how often you want it to update
 
 
-def check_state(state_url):
+def init_vote_counts(states):
     vote_counts = {}
+    for state in states:
+        vote_counts[state] = {'Trump': 0, 'Biden': 0}
+    return vote_counts
+
+
+def print_votes(state):
+    print('VOTE UPDATE')
+    print(datetime.now())
+    print(state)
+    print('Trump votes:', vote_counts[state]['Trump'])
+    print('Biden votes:', vote_counts[state]['Biden'])
+    print("Biden's lead:",
+          vote_counts[state]['Biden'] - vote_counts[state]['Trump'])
+    print('\a\a\a')
+
+
+def check_state(state):
+    state_url = f'https://www.nytimes.com/interactive/2020/11/03/us/elections/results-{state.lower()}-president.html'
     page = requests.get(state_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     table = soup.find('table', class_='e-table e-results-table')
     body = table.find('tbody')
     trs = body.find_all('tr')
+
+    votes_changed = False
     for tr in trs:
         name = tr.find('span', class_='e-name-display').text
-        votes = tr.find('span', class_='e-votes-display').text
-        if 'Trump' in name:
-            vote_counts['Trump'] = int(votes.replace(',', ''))
-        elif 'Biden' in name:
-            vote_counts['Biden'] = int(votes.replace(',', ''))
-    print('Trump votes:', vote_counts['Trump'])
-    print('Biden votes:', vote_counts['Biden'])
-    print("Biden's lead:", vote_counts['Biden'] - vote_counts['Trump'])
+        votes_string = tr.find('span', class_='e-votes-display').text
+        votes = int(votes_string.replace(',', ''))
+        candidate = 'Trump' if 'Trump' in name else 'Biden' if 'Biden' in name else None
+        if candidate:
+            if vote_counts[state][candidate] != votes:
+                votes_changed = True
+            vote_counts[state][candidate] = votes
+    if votes_changed:
+        print_votes(state)
 
 
 def check_states():
-    print(datetime.now())
     for state in states:
-        state_url = f'https://www.nytimes.com/interactive/2020/11/03/us/elections/results-{state.lower()}-president.html'
-        print(state)
-        check_state(state_url)
+        check_state(state)
 
 
+vote_counts = init_vote_counts(states)
 while True:
     check_states()
     sleep(TIME_INTERVAL)
